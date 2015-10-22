bfded1b0dea823040fb8ae8fab94a189310d828a
evgeniy <evgeniy@takeit.to>
2015-10-22 20:59:34 +0300

    add method save() in UserController

--- grails-app/controllers/uawebchallenge/semifinalviii/UserController.groovy
+++ grails-app/controllers/uawebchallenge/semifinalviii/UserController.groovy
@@ -7,9 +7,6 @@ class UserController {
 
     def index() { }
 
-    def save() {
-
-    }
 
     def list() {
         def list = User.list()
@@ -18,5 +15,28 @@ class UserController {
         }catch(Exception e) {
             render(status: 500, contentType: "text/json", text: [status: "error", message: "Some internal error happened on server!"])
         }
+    }
+
+    def save(){
+        try {
+            def params = request.JSON
+            if(params.name && params.email && params.password) {
+
+                def isExist = User.findByEmail(params.email)
+
+                if (!isExist){
+                    def user = new User (name: params.name, email: params.email, password: params.password).save(flush: true, failOnError: true)
+                    render(status: 201, contentType: "text/json", text: [status: "success", data: user] as JSON)
+                }else {
+                    render(status: 409, contentType: "text/json", text: [status: "error", message: "User with this email already exists!"] as JSON)
+                }
+
+            }else {
+                render(status: 400, contentType: "text/json", text: [status: "error", message: "Not enough parameters!"] as JSON)
+            }
+        }catch(Exception e) {
+            render(status: 500, contentType: "text/json", text: [status: "error", message: "Some internal error happened on server!"])
+        }
+
     }
 }
