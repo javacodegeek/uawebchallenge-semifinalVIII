205fb87f14e2751f11077d6ffb20227ec83da686
evgeniy <evgeniy@takeit.to>
2015-10-22 14:54:58 +0300

    create User domain object

---
+++ grails-app/domain/uawebchallenge/semifinalviii/User.groovy
@@ -0,0 +1,32 @@
+package uawebchallenge.semifinalviii
+
+class User {
+
+    String identifier
+    String name
+    String description
+    String email
+    Date dateCreated = new Date()
+    Date dateMofified = new Date()
+
+    static constraints = {
+        identifier(nullable: false, unique: true)
+        name(nullable: false, blank: false)
+        description(nullable: true, blank: true)
+        email(nullable: false, blank: false, unique: true)
+        dateCreated(nullable: false)
+        dateMofified(nullable: false)
+    }
+
+    static mapping = {
+        version false
+        identifier column: identifier, index: 'User_identifier_Idx'
+        name column: name
+        description column: description
+        email column: email
+        dateCreated column: datecreated
+        dateMofified column: datemofified
+    }
+}
+
+
