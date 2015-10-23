f4fdd294f0a4c14d1dc83d4357e206dd20018a26
evgeniy <evgeniy@takeit.to>
2015-10-23 16:30:19 +0300

    create RegistrationService add method create

---
+++ grails-app/services/uawebchallenge/semifinalviii/RegistrationService.groovy
@@ -0,0 +1,19 @@
+package uawebchallenge.semifinalviii
+
+import grails.transaction.Transactional
+
+@Transactional
+class RegistrationService {
+
+    EmailService emailService
+
+    def create(Integer userId,  String userEmail, String userName) {
+        try {
+            def Registration = new Registration(userId: userId, description: "", token: new Date().getTime().encodeAsMD5().toString()).save(flush: true, failOnError: true)
+            emailService.send("Administration <info@uwcua.com>", userEmail, "Registration confirmation", "hello $userName!!!")
+            return Registration
+        }catch(Exception e){
+            return false
+        }
+    }
+}
