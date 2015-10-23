d3e44b30a0ec4f4ac9469935daf96e4f6f6909ff
evgeniy <evgeniy@takeit.to>
2015-10-23 21:50:40 +0300

    minor

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -69,24 +69,28 @@ environments {
     }
     production {
         dataSource {
-            driverClassName = "org.postgresql.Driver"
-            dialect = "org.hibernate.dialect.PostgreSQLDialect"
-            pooled = true
-            type = "com.mchange.v2.c3p0.ComboPooledDataSource"
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             properties {
-                maxActive = -1
-                minEvictableIdleTimeMillis=1800000
-                timeBetweenEvictionRunsMillis=1800000
-                numTestsPerEvictionRun=3
-                testOnBorrow=true
-                testWhileIdle=true
-                testOnReturn=true
-                validationQuery="SELECT 1"
+               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+               jmxEnabled = true
+               initialSize = 5
+               maxActive = 50
+               minIdle = 5
+               maxIdle = 25
+               maxWait = 10000
+               maxAge = 10 * 60000
+               timeBetweenEvictionRunsMillis = 5000
+               minEvictableIdleTimeMillis = 60000
+               validationQuery = "SELECT 1"
+               validationQueryTimeout = 3
+               validationInterval = 15000
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = false
+               jdbcInterceptors = "ConnectionState"
+               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
-            username = 'tzytpqwyizupdz'
-            password = '1IR_RsvRpHejdvkT0ORXGsCPAt'
-            url = "postgres://tzytpqwyizupdz:1IR_RsvRpHejdvkT0ORXGsCPAt@ec2-54-225-194-162.compute-1.amazonaws.com:5432/dcia6nepcslpg5"
-            dbCreate = "create-drop"
         }
     }
 }
