dbdfa6d0554ae1cd6dd62c07029d6390fdc78735
evgeniy <evgeniy@takeit.to>
2015-10-25 09:46:34 +0200

    create Lending Domain Class

---
+++ grails-app/domain/uawebchallenge/semifinalviii/Lending.groovy
@@ -0,0 +1,37 @@
+package uawebchallenge.semifinalviii
+
+class Lending {
+
+    Integer userId
+    String name
+    String projectLink
+    String link
+    String cssStyle
+    String page
+    Date dateCreated
+    Date dateModified
+
+    static constraints = {
+        userId(nullable: false, blank: false)
+        name(nullable: false, blank: false)
+        projectLink(nullable: true, blank: true)
+        link(nullable: true, blank: true)
+        cssStyle(nullable: true, blank: true)
+        page(nullable: true, blank: true)
+        dateCreated(nullable: false, blank: false)
+        dateModified(nullable: false, blank: false)
+
+    }
+
+    static mapping = {
+        version false
+        userId column: 'user_id'
+        name column: 'name'
+        projectLink column: 'project_link'
+        link column: 'link'
+        cssStyle column: 'css_style'
+        page column: 'page'
+        dateCreated column: 'date_created'
+        dateModified column: 'date_modified'
+    }
+}
