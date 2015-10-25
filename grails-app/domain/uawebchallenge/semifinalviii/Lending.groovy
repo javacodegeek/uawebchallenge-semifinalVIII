04f64fcb82d6c972019f993aaa28bf5a09330db3
evgeniy <evgeniy@takeit.to>
2015-10-25 11:58:34 +0200

    Lending Domain:  add needMoney  add totalMoney

--- grails-app/domain/uawebchallenge/semifinalviii/Lending.groovy
+++ grails-app/domain/uawebchallenge/semifinalviii/Lending.groovy
@@ -8,6 +8,8 @@ class Lending {
     String cssStyle
     String page
     String token
+    Integer needMoney = 0
+    Integer totalMoney = 0
     Date dateCreated = new Date()
     Date dateModified = new Date()
 
@@ -18,6 +20,8 @@ class Lending {
         cssStyle(nullable: true, blank: true)
         page(nullable: true, blank: true)
         token(nullable: false, blank: false)
+        needMoney(nullable: false, blank: false)
+        totalMoney(nullable: false, blank: false)
         dateCreated(nullable: false, blank: false)
         dateModified(nullable: false, blank: false)
 
@@ -31,6 +35,8 @@ class Lending {
         cssStyle column: 'css_style', sqlType: "Text"
         page column: 'page', sqlType: "Text"
         token column: 'token'
+        needMoney column: 'need_money'
+        totalMoney column: 'total_money'
         dateCreated column: 'date_created'
         dateModified column: 'date_modified'
     }
