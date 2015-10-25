8625f0bbbc0b3e3ea59f76d79ce17dfe2e9957b6
evgeniy <evgeniy@takeit.to>
2015-10-26 00:38:44 +0200

    ninor fix

--- grails-app/services/uawebchallenge/semifinalviii/RegistrationService.groovy
+++ grails-app/services/uawebchallenge/semifinalviii/RegistrationService.groovy
@@ -10,7 +10,7 @@ class RegistrationService {
     def create(Integer userId,  String userEmail, String userName) {
         try {
             def Registration = new Registration(userId: userId, description: "", token: new Date().getTime().encodeAsMD5().toString()).save(flush: true, failOnError: true)
-            emailService.send("Administration <info@uwcua.com>", userEmail, "Registration confirmation", "hello $userName!!!")
+            emailService.send("Administration <info@uwcua.com>", userEmail, "Registration confirmation", "Hello $userName, you registrated on landing constructor site. Thank you.")
             return Registration
         }catch(Exception e){
             return false
